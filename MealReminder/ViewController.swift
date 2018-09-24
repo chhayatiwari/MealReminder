9e90625bd1e2a698a02be64b541b6bf9fae4cc47
Chhaya Tiwari <apple@Chhaya-Tiwari.local>
2018-09-24 17:04:54 +0530

    Basic Ui for day and Date

--- MealReminder/ViewController.swift
+++ MealReminder/ViewController.swift
@@ -8,14 +8,38 @@
 
 import UIKit
 import FSCalendar
+import Alamofire
 
 class ViewController: UIViewController, FSCalendarDataSource, FSCalendarDelegate, FSCalendarDelegateAppearance {
 
+    @IBOutlet weak var datelabel: UILabel!
+    @IBOutlet weak var calendar: FSCalendar!
+    @IBOutlet weak var calendarHeightConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        calendar.setCurrentPage(Date(), animated: true)
+        self.calendar.accessibilityIdentifier = "calendar"
+        if UIDevice.current.model.hasPrefix("iPad") {
+           // self.calendarHeightConstraint.constant = 400
+        }
+        self.calendar.scope = .week
+        calendar.appearance.caseOptions = FSCalendarCaseOptions.weekdayUsesSingleUpperCase
+        calendar.appearance.titleWeekendColor = UIColor.gray
+        self.calendar.select(Date())
     }
 
+    fileprivate lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.locale = Locale(identifier: "en_US")
+        formatter.setLocalizedDateFormatFromTemplate("EEEE, ddMMMMyyyy")
+        return formatter
+    }()
 
+    func calendar(_ calendar: FSCalendar, boundingRectWillChange bounds: CGRect, animated: Bool) {
+        self.calendarHeightConstraint.constant = bounds.height
+        self.view.layoutIfNeeded()
+    }
 }
 
